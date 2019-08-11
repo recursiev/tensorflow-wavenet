3cca2ccbb3c0de13b3341f9d858a338d656880ed
Erik <erik@MacBook-Pro-4.local>
2019-08-11 13:08:39 -0400

    b

--- wavenet/audio_reader.py
+++ wavenet/audio_reader.py
@@ -8,7 +8,7 @@ import librosa
 import numpy as np
 import tensorflow as tf
 
-FILE_PATTERN = r'p([0-9]+)_([0-9]+)\.wav'
+FILE_PATTERN = r'([0-9]{1,2})-([0-9]+)-([A-Z]{1})-([0-9]+)\.wav'
 
 
 def get_category_cardinality(files):
